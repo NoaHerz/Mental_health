11781a24c8c3eb8ed213bfdc6a87c3adeb5c0c86
Noa Herz herz.noa@gmail.com <noaherz@rhino2.rhino.psych.upenn.edu>
2022-02-28 16:37:36 -0500

    noa's ieeg edits'

---
+++ .ipynb_checkpoints/imports-checkpoint.py
@@ -0,0 +1,72 @@
+import numpy as np
+import pandas as pd
+
+#import. The CMLReader class is your gateway to all experimental data, including electrodes and EEG. The get_data_index function specifically loads experimental databases. 
+from cmlreaders import CMLReader, get_data_index
+
+import pickle
+import os
+import pandas as pd
+import numpy as np
+import scipy.signal
+from scipy.stats import zscore
+from cmlreaders import CMLReader, get_data_index
+from pylab import *
+from correctEEGoffset import *
+from convertMstoWindowNumber import convertMstoWindowNumber, ms2WindowNumber
+from add_output_position import add_output_position
+
+import statsmodels.api as sm
+from scipy import stats
+
+import gensim.models as models
+from scipy import spatial
+from scipy.io import loadmat
+from scipy.stats import sem
+import os, csv, numpy, pandas
+import pandas as pd
+import numpy as np
+from sklearn import preprocessing
+import seaborn as sns
+
+from statsmodels.stats.anova import AnovaRM
+from lrtest import lrtest
+from add_semantic_relatedness import add_semantic_relatedness, categorized_words
+
+from ptsa.data.filters import ButterworthFilter
+from ptsa.data.filters import MorletWaveletFilter
+from regionalizationModule import get_elec_regions,Loc2PairsTranslation
+import xarray as xr
+from getMTLregions import getMTLregions
+
+import numpy as np
+import pandas as pd
+from cmlreaders import CMLReader, get_data_index
+import pickle
+
+
+import scipy.signal
+from scipy.stats import zscore
+from correctEEGsubjects import correctEEGsubjects
+from correctEEGoffset import correctEEGoffset
+from getElecCats import getElecCats
+from missing_channels import missing_channels
+from getMTLregions import getMTLregions
+from getBadChannels import getBadChannels
+from event_type_index import event_type_index
+from add_semantic_similarity import add_semantic_similarity
+from add_semantic_similarity import case_insensitive_similarity
+from add_semantic_similarity import categorized_words
+from add_output_position import add_output_position
+from brain_label import brain_label
+
+from add_semantic_relatedness import add_semantic_relatedness,categorized_words
+import gensim.models as models
+import os, csv, numpy, pandas
+
+from ptsa.data.filters import ButterworthFilter
+from ptsa.data.filters import MorletWaveletFilter
+from add_mirror_buffer_adjusted import add_mirror_buffer_adjusted #
+from regionalizationModule import get_elec_regions,Loc2PairsTranslation
+import xarray as xr
+from average_tilt import average_tilt
